e337fa0a16f62319abb60c2b16352dfc84556e78
Abdelrahman Mohamed <36246909+Mehrizz@users.noreply.github.com>
2020-01-28 23:15:54 +0200

    Add files via upload

---
+++ A3/src/assignment3/ProcessGUI.java
@@ -0,0 +1,20 @@
+package assignment3;
+
+public class ProcessGUI
+{
+    public String name;
+    public String color;
+    public int start;
+    public int end;
+
+    public ProcessGUI()
+    {
+
+    }
+    public void test()
+    {
+        System.out.println("Name: "+ name + " - Color: " + color
+                + " start: " +  start + " end: " +
+                end);
+    }
+}
